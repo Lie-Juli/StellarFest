8530a122c8c83085b34ab908cd32bc59b904bd9f
Julius Lie <juliusliejulius@gmail.com>
2024-12-02 14:21:56 +0700

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module StellarFest {
+	requires java.sql;
+	requires javafx.graphics;
+	requires javafx.base;
+	requires javafx.controls;
+	
+	opens main;
+}
---
+++ src/util/Connect.java
@@ -0,0 +1,70 @@
+package util;
+
+import java.sql.*;
+
+public class Connect {
+	
+	private final String USERNAME = "root";
+	private final String PASSWORD = "";
+	private final String HOST = "localhost:3306";
+	private final String DATABASE = "ooad_sesi9";
+	private final String CONNECTION = String.format("jdbc:mysql://%s/%s", HOST, DATABASE);
+	
+	public ResultSet rs;
+	public ResultSetMetaData rsm;
+	
+	private Connection con;
+	private Statement st;
+
+	private static Connect connect;
+	
+	public static Connect getInstance(){
+		if(connect == null) {
+			connect = new Connect();
+		}
+		return connect;
+	}
+	
+	private Connect() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(CONNECTION, USERNAME, PASSWORD);
+			st = con.createStatement();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public ResultSet execQuery(String query) {
+		try {
+			rs = st.executeQuery(query);
+			rsm = rs.getMetaData();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	public void execUpdate(String query) {
+		try {
+			st.executeUpdate(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public PreparedStatement prepareStatement(String query) {
+		PreparedStatement ps = null;
+		
+		try {
+			ps = con.prepareStatement(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ps;
+	}
+}
